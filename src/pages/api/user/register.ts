6fb8b67c0d9549c33a276fe6ada962c5a24bc6c0
Akbal-21 <akbal153@hotmail.com>
2023-09-26 14:02:40 -0600

    teacher exercise 2

--- src/pages/api/user/register.ts
+++ src/pages/api/user/register.ts
@@ -1,108 +1,108 @@
-import { db } from "@/db";
-import { IUser } from "@/interface";
-import { isValidEmail, jwt } from "@/utils";
-import bcrypt from "bcryptjs";
-import type { NextApiRequest, NextApiResponse } from "next";
+// import { db } from "@/db";
+// import { IUser } from "@/interface";
+// import { isValidEmail, jwt } from "@/utils";
+// import bcrypt from "bcryptjs";
+// import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data =
-  | {
-      message: string;
-    }
-  | {
-      token: string;
-      user: {
-        name: string;
-        last_name: string;
-        email: string;
-      };
-    };
+// type Data =
+//   | {
+//       message: string;
+//     }
+//   | {
+//       token: string;
+//       user: {
+//         name: string;
+//         last_name: string;
+//         email: string;
+//       };
+//     };
 
-export default function (req: NextApiRequest, res: NextApiResponse<Data>) {
-  switch (req.method) {
-    case "POST":
-      return registerUser(req, res);
+// export default function (req: NextApiRequest, res: NextApiResponse<Data>) {
+//   switch (req.method) {
+//     case "POST":
+//       return registerUser(req, res);
 
-    default:
-      return res.status(400).json({ message: "Bad request" });
-  }
-}
+//     default:
+//       return res.status(400).json({ message: "Bad request" });
+//   }
+// }
 
-async function registerUser(req: NextApiRequest, res: NextApiResponse<Data>) {
-  const {
-    name = "",
-    last_name = "",
-    email = "",
-    password = "",
-  } = req.body as {
-    name: string;
-    last_name: string;
-    email: string;
-    password: string;
-  };
+// async function registerUser(req: NextApiRequest, res: NextApiResponse<Data>) {
+//   const {
+//     name = "",
+//     last_name = "",
+//     email = "",
+//     password = "",
+//   } = req.body as {
+//     name: string;
+//     last_name: string;
+//     email: string;
+//     password: string;
+//   };
 
-  // return res.status(200).json({ message: "Hola" });
+//   // return res.status(200).json({ message: "Hola" });
 
-  if (password.length < 6) {
-    return res
-      .status(400)
-      .json({ message: "La contreseña debe de ser de 6 caracteres" });
-  }
+//   if (password.length < 6) {
+//     return res
+//       .status(400)
+//       .json({ message: "La contreseña debe de ser de 6 caracteres" });
+//   }
 
-  if (name.length < 3) {
-    return res
-      .status(400)
-      .json({ message: "El nombre debe de ser de 2 caracteres" });
-  }
+//   if (name.length < 3) {
+//     return res
+//       .status(400)
+//       .json({ message: "El nombre debe de ser de 2 caracteres" });
+//   }
 
-  if (!isValidEmail(email)) {
-    return res.status(400).json({ message: "El ocrreo debe de ser valido" });
-  }
+//   if (!isValidEmail(email)) {
+//     return res.status(400).json({ message: "El ocrreo debe de ser valido" });
+//   }
 
-  if (last_name.length <= 6) {
-    return res
-      .status(400)
-      .json({ message: "El apellido debe de ser de 6 caracteres" });
-  }
+//   if (last_name.length <= 6) {
+//     return res
+//       .status(400)
+//       .json({ message: "El apellido debe de ser de 6 caracteres" });
+//   }
 
-  await db.prisma.$connect();
+//   await db.prisma.$connect();
 
-  const user = await db.prisma.usuarios.findUnique({
-    where: { Correo: email },
-  });
+//   const user = await db.prisma.usuarios.findUnique({
+//     where: { Correo: email },
+//   });
 
-  if (user) {
-    return res.status(400).json({
-      message: "No puede usar ese correo",
-    });
-  }
-  let newUser: IUser;
+//   if (user) {
+//     return res.status(400).json({
+//       message: "No puede usar ese correo",
+//     });
+//   }
+//   let newUser: IUser;
 
-  try {
-    newUser = await db.prisma.usuarios.create({
-      data: {
-        Nombres: name,
-        Apellidos: last_name,
-        Contrasena: bcrypt.hashSync(password),
-        Correo: email,
-      },
-    });
-  } catch (error) {
-    return res
-      .status(400)
-      .json({ message: `revisar los logs del servidor ${error}` });
-  }
+//   try {
+//     newUser = await db.prisma.usuarios.create({
+//       data: {
+//         Nombres: name,
+//         Apellidos: last_name,
+//         Contrasena: bcrypt.hashSync(password),
+//         Correo: email,
+//       },
+//     });
+//   } catch (error) {
+//     return res
+//       .status(400)
+//       .json({ message: `revisar los logs del servidor ${error}` });
+//   }
 
-  await db.prisma.$disconnect();
+//   await db.prisma.$disconnect();
 
-  const { Usuarios_id } = newUser;
-  const token = jwt.signToken(Usuarios_id, email);
+//   const { Usuarios_id } = newUser;
+//   const token = jwt.signToken(Usuarios_id, email);
 
-  return res.status(200).json({
-    token, // *JWT
-    user: {
-      name,
-      email,
-      last_name,
-    },
-  });
-}
+//   return res.status(200).json({
+//     token, // *JWT
+//     user: {
+//       name,
+//       email,
+//       last_name,
+//     },
+//   });
+// }
