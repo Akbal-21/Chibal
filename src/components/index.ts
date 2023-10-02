1508615dddad3d7f3bf12866ed713f53d05a8c7e
AnsgarGM <loxn52@gmail.com>
2023-10-02 14:46:35 -0600

    Inicio Alumno, faltan botones

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export * from "./admin";
 export * from "./layouths";
 export * from "./teacher";
 export * from "./ui";
+export * from "./student";
---
+++ src/pages/api/student/[StudentID].ts
@@ -0,0 +1,51 @@
+import { db } from "@/db";
+import { IExerciseDB } from "@/interface";
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data =
+  | {
+      message: string;
+    }
+  | {
+      dataExercise: IExerciseDB[];
+    };
+
+export default function (req: NextApiRequest, res: NextApiResponse<Data>) {
+    switch (req.method) {
+        case "GET":
+            return getExercisesByStudent(req, res) 
+
+    
+        default:
+            return res.status(400).json({ message: 'Bad request' })
+            
+    }
+}
+
+async function getExercisesByStudent(req: NextApiRequest, res: NextApiResponse<Data>) {
+    const { StudentID } = req.query;
+    await db.prisma.$connect();
+    // Primero obtener el grupo del niño y despues lso ejercicios segun el grupo
+    const grupoId = await db.prisma.alumnos.findUnique({
+        where: {
+          Usuario_id: Number(StudentID),
+        },
+        select: {
+          Grupo_id: true,
+        },
+      });
+  
+    const dataExercise: IExerciseDB[] = await db.prisma.ejercicios.findMany({
+        where: {
+            GrupoID: grupoId?.Grupo_id,
+        },
+    });
+
+    await db.prisma.$disconnect();
+
+    if (!dataExercise) {
+        return res.status(404).json({ message: "Bad Request" });
+    }
+    
+    return res.status(200).json({ dataExercise });
+}
