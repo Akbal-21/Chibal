0ba0d889abc1c81731c5f6d1f152a3231188f984
francisco-renteria <luisfrancisco.renteria@gmail.com>
2023-10-08 20:31:48 -0600

    canvas final

--- src/pages/student/exercise/[slug].tsx
+++ src/pages/student/exercise/[slug].tsx
@@ -2,24 +2,26 @@ import React, { useEffect, useState } from "react";
 import { getExerciseQuestions } from "@/api/getJson";
 import { GetServerSideProps, NextPage } from "next";
 
-import App from './App';
+import App from "./App";
 
 // El componente App se exporta como valor por defecto, por lo que no se necesita "import { App }"
 interface Props {
   results: string;
-  slug : string;
+  slug: string;
 }
-const solve: NextPage<Props> = ({ results,slug }) => {
+const solve: NextPage<Props> = ({ results, slug }) => {
   console.log(JSON.parse(results));
-  console.log(typeof(results))
-  return (<div>{results}
-  
-  <React.StrictMode>
-    <App slug={slug} results={results}/>
-  </React.StrictMode>
-  
-  </div>);
-  
+  console.log(typeof results);
+
+  return (
+    <div>
+      {results}
+
+      <React.StrictMode>
+        <App slug={slug} results={results} />
+      </React.StrictMode>
+    </div>
+  );
 };
 
 export default solve;
@@ -27,12 +29,23 @@ export default solve;
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   const { slug = "" } = query as { slug: string };
   const results: string = await getExerciseQuestions(Number(slug));
-  console.log(slug)
-  console.log(results)
-  return {
-    props: {
-      results,
-      slug
-    },
-  };
+  console.log(slug);
+  console.log(results);
+
+  if (Number(slug)){
+    return {
+      props: {
+        results,
+        slug,
+      },
+    };
+  }
+  else {
+    return {
+      redirect: {
+        destination: "/student",
+        permanent: false,
+      },
+    };
+  }
 };
