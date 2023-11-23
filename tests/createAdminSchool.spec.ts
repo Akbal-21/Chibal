14553e3ac744fd38efbc7428fff24f247522a7c3
AnsgarGM <loxn52@gmail.com>
2023-11-22 22:40:07 -0600

    Commit para queso

---
+++ tests/createAdminSchool.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from "@playwright/test";
+
+test( "Crea la escuela y el administrador de la misma escuela", async ( { page } ) => {
+    // * Login
+    await page.goto("http://localhost:3000/auth/login");
+    await page.getByPlaceholder("Enter email").fill("su@admin.com");
+    await page.getByPlaceholder("Enter password").fill("123456");
+    await page.getByRole("button", { name: "Ingresar" }).click();
+
+    // * Crea administrador
+    await page.getByText('Administradores').click();
+    await page.getByRole('button', { name: 'Nuevo Administrador' }).click();
+    await page.getByLabel('Nombre').fill('Administrador');
+    await page.getByLabel('Apellidos').fill('Nuevo');
+    await page.getByLabel('Correo').fill('emilio@admin.com');
+    await page.getByRole('button', { name: 'Guardar' }).click();
+    await expect( page ).toHaveURL( new RegExp( "/superAdmin/admins" ) );
+
+    // * Crea escuela
+    await page.getByText('Escuelas').click();
+    await page.getByRole('button', { name: 'Nueva escuela' }).click();
+    await page.getByLabel('Nombre').fill('Escuela Nueva');
+    await page.locator('label').filter({ hasText: 'Seleccione un administrador' }).click();
+    await page.getByText('Administrador Nuevo').click();
+    await page.getByRole('button', { name: 'Guardar' }).click();
+    await expect( page ).toHaveURL( new RegExp( "/superAdmin/schools" ) );
+} );
