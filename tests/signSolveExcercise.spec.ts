472d3640322efce716128cff774f3cf48806a0d0
francisco-renteria <luisfrancisco.renteria@gmail.com>
2023-11-27 19:47:31 -0600

    Pruebas trazo 1,7 y Modelo Quantized

--- tests/signSolveExcercise.spec.ts
+++ tests/signSolveExcercise.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect, Page } from "@playwright/test";
 
-test( "Crea un profesor para la escuela", async ( { page } ) => {
+test( "Iniciar sesion del alumno y resolver ejercicio", async ( { page } ) => {
     // * Login
     await page.goto("http://localhost:3000/auth/login");
     await page.getByPlaceholder("Correo electronico").fill("carlitos@alumno.com");
@@ -13,7 +13,7 @@ test( "Crea un profesor para la escuela", async ( { page } ) => {
     await page.getByRole('button', { name: '¡Empezar a responder!' }).click();
 
     await page.waitForTimeout(1500);
-    await seven(page);
+    await one(page);
     await page.getByRole("button", { name: "Siguiente" }).click();
 
     await page.waitForTimeout(1500);
@@ -21,7 +21,7 @@ test( "Crea un profesor para la escuela", async ( { page } ) => {
     await page.getByRole("button", { name: "Finalizar" }).click();
 
     await expect(page.locator("footer")).toHaveText(
-        "✅ 4 Correctas - ❌ 0 Incorrectas - ❓ 0 Sin responder",
+        "✅ 2 Correctas - ❌ 0 Incorrectas - ❓ 0 Sin responder",
         { timeout: 4000 }
       );
     
@@ -48,4 +48,30 @@ async function seven(page: Page) {
       await page.mouse.up();
       await page.waitForTimeout(1000);
     }
+}
+
+async function one(page: Page) {
+  const canvas = await page.getByTitle("Dibuja");
+  const pos = await canvas.boundingBox();
+
+  if (pos) {
+    await page.waitForTimeout(25);
+    await page.mouse.move(pos.x + 74, pos.y + 68, { steps: 20 });
+    await page.waitForTimeout(25);
+    await page.mouse.down();
+    await page.waitForTimeout(25);
+    await page.mouse.move(pos.x + 127, pos.y + 32, { steps: 20 });
+    await page.waitForTimeout(25);
+    await page.mouse.move(pos.x + 130, pos.y + 204, { steps: 20 });
+    await page.waitForTimeout(25);
+
+    await page.mouse.move(pos.x + 57, pos.y + 207, { steps: 20 });
+    await page.waitForTimeout(25);
+
+    await page.mouse.move(pos.x + 189, pos.y + 210, { steps: 20 });
+    await page.waitForTimeout(25);
+
+    await page.mouse.up();
+    await page.waitForTimeout(1000);
+  }
 }
